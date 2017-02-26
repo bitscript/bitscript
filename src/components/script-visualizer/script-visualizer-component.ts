e38815b49cc5a0c74ee8a0a513fe392b59fc56ee
Jason Dreyzehner <jason@dreyzehner.com>
2017-02-26 03:17:53 -0500

    move to script-visualizer

--- src/components/script-visualizer/script-visualizer-component.ts
+++ src/components/script-visualizer/script-visualizer-component.ts
@@ -8,13 +8,39 @@ import { NavParams } from 'ionic-angular/navigation/nav-params';
 import { NavOptions } from 'ionic-angular/navigation/nav-util';
 import { ViewController } from 'ionic-angular/navigation/view-controller';
 
+import { ScriptKeyboardController } from '../../components/script-keyboard/script-keyboard';
+
 @Component({
   selector: 'script-visualizer',
   templateUrl: 'script-visualizer-component.html',
   encapsulation: ViewEncapsulation.None
 })
 export class ScriptVisualizerCmp {
-
-  constructor() {
+  scriptKeyboard;
+  operationList: any;
+  constructor(private scriptKeyboardController: ScriptKeyboardController) {
+    this.scriptKeyboard = scriptKeyboardController.create(this.handleKeyPress);
+    
+    this.operationList = [
+      {
+        operation: 'OP_2',
+        stackBefore: [],
+        stackAfter: [2]
+      },
+      {
+        operation: 'OP_3',
+        stackBefore: [2],
+        stackAfter: [2, 3]
+      },
+      {
+        operation: 'OP_ADD',
+        stackBefore: [2, 3],
+        stackAfter: [5]
+      },
+    ]
+  }
+  
+  handleKeyPress(opcode) {
+    console.log('key pressed', opcode)
   }
 }
